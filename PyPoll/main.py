600c9596d7660f1a8b28a04a576a82cf2df91c25
Mitchell Capell <51679181+mitchellcapell95@users.noreply.github.com>
2019-09-17 21:19:20 -0400

    Add files via upload

---
+++ PyPoll/main.py
@@ -0,0 +1,96 @@
+import os
+
+import csv
+
+# csvpath = os.path.join('Desktop','UCFLM201907DATA2','03-Python', 'Homework','Instructions','PyPoll','Resources','election_data.csv')
+
+csvpath = "election_data.csv"
+
+counter = 0
+candidate_Khan = 0
+candidate_Correy = 0
+candidate_Li = 0
+candidate_Tooley = 0
+
+
+list_of_candidates = []
+
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile,delimiter=',')
+    if csv.Sniffer().has_header:
+        next(csvreader)
+
+    for row in csvreader:
+        if str(row[2]) not in list_of_candidates:
+            list_of_candidates.append(str(row[2]))
+            
+list_of_candidates_string = ', '.join(list_of_candidates)
+
+
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile,delimiter=',')
+    if csv.Sniffer().has_header:
+        next(csvreader)
+
+    for row in csvreader:
+        counter += 1
+        candidate_votes = str(row[2])
+        if candidate_votes == "Khan":
+            candidate_Khan += 1
+        
+        elif candidate_votes == "Correy":
+            candidate_Correy +=1
+            
+        elif candidate_votes == "Li":
+            candidate_Li += 1
+            
+        else:
+            candidate_Tooley += 1
+            
+percentage_Khan = round(((candidate_Khan / counter) * 100),2)
+percentage_Correy = round(((candidate_Correy / counter) * 100),2)
+percentage_Li = round(((candidate_Li / counter) * 100),2)
+percentage_Tooley = round(((candidate_Tooley / counter) * 100),2)
+
+        
+print("The candidates are: " + str(list_of_candidates_string))
+print("Total Votes: " + str(counter))
+print("Khan: " + str(percentage_Khan) + "% " + "(" + str(candidate_Khan) + ")")
+print("Correy: " + str(percentage_Correy) + "% " + "(" + str(candidate_Correy) + ")")
+print("Li: " + str(percentage_Li) + "% " + "(" + str(candidate_Li) + ")")
+print("O'Tooley: " + str(percentage_Tooley) + "% " + "(" + str(candidate_Tooley) + ")")
+
+if candidate_Khan > candidate_Correy and candidate_Li and candidate_Tooley:
+    print("Winner: Khan")
+    
+elif candidate_Correy > candidate_Khan and candidate_Li and candidate_Tooley:
+    print("Winner: Correy")
+    
+elif candidate_Li > candidate_Correy and candidate_Khan and candidate_Tooley:
+    print("Winner: Li")
+    
+else:
+    print("Winner: O'Tooley")
+
+
+text_file = open("election_results.txt", "w")
+text_file.write("The candidates are: " + str(list_of_candidates_string) + "\n")
+text_file.write("Total Votes: " + str(counter) + "\n")
+text_file.write("Khan: " + str(percentage_Khan) + "% " + "(" + str(candidate_Khan) + ")" + "\n")
+text_file.write("Correy: " + str(percentage_Correy) + "% " + "(" + str(candidate_Correy) + ")" + "\n")
+text_file.write("Li: " + str(percentage_Li) + "% " + "(" + str(candidate_Li) + ")" + "\n")
+text_file.write("O'Tooley: " + str(percentage_Tooley) + "% " + "(" + str(candidate_Tooley) + ")" + "\n")
+
+if candidate_Khan > candidate_Correy and candidate_Li and candidate_Tooley:
+    text_file.write("Winner: Khan")
+    
+elif candidate_Correy > candidate_Khan and candidate_Li and candidate_Tooley:
+    text_file.write("Winner: Correy")
+    
+elif candidate_Li > candidate_Correy and candidate_Khan and candidate_Tooley:
+    text_file.write("Winner: Li")
+    
+else:
+    text_file.write("Winner: O'Tooley")
